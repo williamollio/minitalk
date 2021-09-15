bd37e05a6ee356dfead68187ff82861332218194
William Ollio <wollio@1-C-2.42heilbronn.de>
2021-09-15 20:51:35 +0200

    sigaction ok

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: wollio <wollio@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/10 16:14:22 by wollio            #+#    #+#             */
-/*   Updated: 2021/09/15 19:57:27 by wollio           ###   ########.fr       */
+/*   Updated: 2021/09/15 20:17:44 by wollio           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,31 +16,23 @@
 
 static	t_server server;
 
-void	ft_handler0()
-{
-	server.flag = 0;
-	server.bit--;
-	server.c += (0 << server.bit);
 
+void	ft_handler(int sig, siginfo_t *siginfo, void *context)
+{
+	printf ("Sending PID: %ld, UID: %ld\n",(long)siginfo->si_pid, (long)siginfo->si_uid);
+
+	(void)context;
+	if (sig == SIGUSR1)
+		server.flag = 0;
+	else if (sig == SIGUSR2)
+		server.flag = 1;
+	server.bit--;
+	server.c += (server.flag << server.bit);
 	if (server.bit == 0)
 	{
 		ft_putchar_fd(server.c, 1);
 		server.bit = 8;
 		server.c = 0;
-	}
-}
-
-void	ft_handler1()
-{
-	server.flag = 1;
-	server.bit--;
-	server.c += (1 << server.bit);
-	if (server.bit == 0)
-	{
-		ft_putchar_fd(server.c, 1);
-		server.bit = 8;
-		server.c = 0;
-
 	}
 }
 
@@ -48,8 +40,16 @@ int	main()
 {
 	pid_t	pid;
 
+	struct sigaction s_signal;
+
+	/* Use the sa_sigaction field because the handles has two additional parameters */
+	s_signal.sa_sigaction = &ft_handler;
+
+	/* The SA_SIGINFO flag tells sigaction() to use the sa_sigaction field, not sa_handler. */
+	s_signal.sa_flags = SA_SIGINFO;
+
+
 	server.bit = 8;
-	server.flag = 2;
 	server.c = 0;
 	pid = getpid();
 
@@ -59,9 +59,10 @@ int	main()
 
 	while(1)
 	{
-		signal(SIGUSR1,ft_handler0);
-		signal(SIGUSR2,ft_handler1);
+		sigaction(SIGUSR1, &s_signal, NULL);
+		sigaction(SIGUSR2, &s_signal, 0);
 		pause();
 	}
+
 	return 0;
 }
